6491e586fac85994b11cc8e164c7a4a80da0e826
Jason Nicoletti <j.nicoletti@wavenet.be>
2024-03-16 19:19:03 +0100

    some improvement

--- src/components/account/AccountPage.tsx
+++ src/components/account/AccountPage.tsx
@@ -1,23 +1,28 @@
-import { Button } from '../../shadcn/components/ui/button.tsx';
-import { BEARER_KEY, USER_KEY } from '../../constants/constants.ts';
-import { useNavigate } from 'react-router-dom';
-import RouterKeys from '../../routes/routerKeys.ts';
+import { Button } from "../../shadcn/components/ui/button.tsx";
+import { BEARER_KEY, USER_KEY } from "../../constants/constants.ts";
+import { useNavigate } from "react-router-dom";
+import RouterKeys from "../../routes/routerKeys.ts";
+import { GithubButton } from "./GithubButton.tsx";
 
-export function AccountPage () {
-    
-    const navigate = useNavigate();
-    
-    const handleDisconnect = () => {
-        sessionStorage.removeItem(BEARER_KEY);
-        sessionStorage.removeItem(USER_KEY);
-        navigate(`/${RouterKeys.Login}`);
-    };
-    
-    return (
-        <div className="main-container h-full relative">
-            <Button onClick={handleDisconnect}
-                    className="absolute bottom-5 right-0"
-                    variant="destructive">Déconnexion</Button>
-        </div>
-    );
+export function AccountPage() {
+	const navigate = useNavigate();
+
+	const handleDisconnect = () => {
+		sessionStorage.removeItem(BEARER_KEY);
+		sessionStorage.removeItem(USER_KEY);
+		navigate(`/${RouterKeys.Login}`);
+	};
+
+	return (
+		<div className="main-container h-full relative">
+			<GithubButton />
+			<Button
+				onClick={handleDisconnect}
+				className="absolute bottom-5 right-0"
+				variant="destructive"
+			>
+				Déconnexion
+			</Button>
+		</div>
+	);
 }
--- src/components/home-page/MainSearchBar.tsx
+++ src/components/home-page/MainSearchBar.tsx
@@ -1,11 +1,10 @@
-import { SearchBar } from '../search-results/SearchBar.tsx';
+import { SearchBar } from "../search-results/SearchBar.tsx";
 
-export default function MainSearchBar () {
-    
-    return (
-        <div className="flex flex-col flex-1 items-center h-full">
-            <h1 className="main-title">🦄 Unicraft 🛠️</h1>
-            <SearchBar isMainSearch/>
-        </div>
-    );
+export default function MainSearchBar() {
+	return (
+		<div className="flex flex-col flex-1 items-center h-full">
+			<h1 className="main-title">🦄 Unicraft 🛠️</h1>
+			<SearchBar isMainSearch />
+		</div>
+	);
 }
